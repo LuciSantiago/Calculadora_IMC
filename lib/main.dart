090a89232514b38cb345bde82edc666d4d879072
LuciSantiago <lucianosantiago.dev@gmail.com>
2022-09-20 10:15:29 -0300

    app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:imc/app_widgets.dart';
-import 'package:imc/features/imc/controller/imc_controller.dart';
-import 'package:imc/features/imc/imc_page.dart';
 
 void main() {
   runApp(const AppWidgets());
